3f83c964a83d5bbd2addf8585df38fcb6dc96ac1
juliantsai971010 <69131692+juliantsai971010@users.noreply.github.com>
2020-08-04 16:59:34 +0800

    Add files via upload

---
+++ day2-4.py
@@ -0,0 +1,6 @@
+a=['ball','apple','tennis','sleep']
+
+a.append('home')
+a.insert(2,'9')
+a.remove('sleep')
+print(a)
---
+++ day2-6.py
@@ -0,0 +1,33 @@
+A=[]
+B=[]
+
+c=input('how many people is there')
+c=int(c)
+for d in range(c):
+    a=input('enter the score')
+    A.append(a)
+a=int(a)
+for d in range(c):
+    b=input('enter the name')
+    b=int(b)
+    B.append(b)
+
+
+if d in range(1):
+    total=total+A
+    print('總分是',total)
+d=int(d)
+f=0
+
+A=a       
+B=b    
+total=0
+#high=0
+#low=0
+    
+    
+    
+    
+
+#
+#    
